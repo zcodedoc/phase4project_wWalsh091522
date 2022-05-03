8c33300ad8dc974c225031a675303fd86479eeb6
William Walsh <wmdwalsh0@gmail.com>
2022-05-02 21:32:14 -0400

    config/

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :workouts
+  resources :users
+  # resources :sessions
+  get "/me", to: "users#show"
+  patch "/workouts/:id", to: "workouts#update"
+  patch "/workouts/:id/like", to: "workouts#increment_likes"
+  post "/signup", to: "users#create"
+  post "/workouts", to: "workouts#create"
+  get "/workouts", to: "workouts#index"
+  get "/workouts/:id", to: "workouts#show"
+  post "/comments", to: "comments#create"
+  # get "/comments", to: "comments#index"
+  # get "/comments/:id", to: "comments#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
